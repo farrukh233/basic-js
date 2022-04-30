0c1889f304c8ac1a211305842df83a59fe92f16c
farrukh <farrukhx50@gmail.com>
2022-04-30 20:22:17 +0500

    fix: add another tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = n.toString().split("");
+  for (let i = 0; i <= arr.length; i++) {
+    if (n >= 100) {
+      if (arr[i] < arr[i + 1]) {
+        delete arr[i];
+      }
+    } else if (n < 100) {
+      let min = Math.min(...arr);
+      let arrMin = arr.filter(e => e != min);
+      return Number(arrMin.join(""));
+    }
+  }
+
+  return Number(arr.join(""));
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(numbers) {
+  let indices = [];
+  numbers
+    .filter((v, i) => v !== -1 && indices.push(i))
+    .sort((a, b) => a - b)
+    .forEach((v, i) => (numbers[indices[i]] = v));
+
+  return numbers;
+
   // remove line with error and write your code here
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
