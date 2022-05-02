760255f5497d571254c93516a7b1667adc8f3015
farrukh <farrukhx50@gmail.com>
2022-05-03 00:52:49 +0500

    what a season

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(d) {
+  let seasonArray = [
+    {
+      name: "Spring",
+      date: new Date(
+        d.getFullYear(),
+        2,
+        d.getFullYear() % 4 === 0 ? 19 : 20
+      ).getTime(),
+    },
+    {
+      name: "Summer",
+      date: new Date(
+        d.getFullYear(),
+        5,
+        d.getFullYear() % 4 === 0 ? 20 : 21
+      ).getTime(),
+    },
+    {
+      name: "Autumn",
+      date: new Date(
+        d.getFullYear(),
+        8,
+        d.getFullYear() % 4 === 0 ? 22 : 23
+      ).getTime(),
+    },
+    {
+      name: "Winter",
+      date: new Date(
+        d.getFullYear(),
+        11,
+        d.getFullYear() % 4 === 0 ? 20 : 21
+      ).getTime(),
+    },
+  ];
+
+  const season = seasonArray.filter(({ date }) => date <= d).slice(-1)[0] || {
+    name: "Winter",
+  };
+  return new Date(d).toLocaleDateString(), season.name;
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
